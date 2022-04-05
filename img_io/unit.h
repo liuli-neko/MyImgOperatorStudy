6e9ca6cda123ec744e855eb6f1ff4d5af8ccf4b2
llhsdmd <2982886437@qq.com>
2022-04-05 15:12:40 +0800

    add butterworth filter and gaussian filter

--- img_io/unit.h
+++ img_io/unit.h
@@ -5,35 +5,80 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-namespace MY_IMG
-{
+namespace MY_IMG {
 
 #define GRAY_MAX 255
-#define LOG(format, ...) printf("[%s:%d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG(format, ...)                                                       \
+  printf("[%s:%d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
-    const cv::Mat H1 = (cv::Mat_<double>(3, 3) << 0, 1, 0, 1, -4, 1, 0, 1, 0);
-    const cv::Mat H2 = (cv::Mat_<double>(3, 3) << 1, 1, 1, 1, -8, 1, 1, 1, 1);
-    const cv::Mat H3 = (cv::Mat_<double>(3, 3) << 1, -2, 1, -2, 4, -2, 1, -2, 1);
-    const cv::Mat H4 = (cv::Mat_<double>(3, 3) << 0, -1, 0, -1, 4, -1, 0, -1, 0);
-    const cv::Mat H5 = (cv::Mat_<double>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1);
-    const cv::Mat H6 = (cv::Mat_<double>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-    const cv::Mat H7 = (cv::Mat_<double>(3, 3) << -1, -1, -1, -1, 9, -1, -1, -1, -1);
-    const cv::Mat H8 = (cv::Mat_<double>(3, 3) << 0, 1, 0, 1, -5, 1, 0, 1, 0);
-    const cv::Mat H9 = (cv::Mat_<double>(3, 3) << 1, 1, 1, 1, -9, 1, 1, 1, 1);
+const cv::Mat H1 = (cv::Mat_<double>(3, 3) << 0, 1, 0, 1, -4, 1, 0, 1, 0);
+const cv::Mat H2 = (cv::Mat_<double>(3, 3) << 1, 1, 1, 1, -8, 1, 1, 1, 1);
+const cv::Mat H3 = (cv::Mat_<double>(3, 3) << 1, -2, 1, -2, 4, -2, 1, -2, 1);
+const cv::Mat H4 = (cv::Mat_<double>(3, 3) << 0, -1, 0, -1, 4, -1, 0, -1, 0);
+const cv::Mat H5 =
+    (cv::Mat_<double>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1);
+const cv::Mat H6 = (cv::Mat_<double>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+const cv::Mat H7 =
+    (cv::Mat_<double>(3, 3) << -1, -1, -1, -1, 9, -1, -1, -1, -1);
+const cv::Mat H8 = (cv::Mat_<double>(3, 3) << 0, 1, 0, 1, -5, 1, 0, 1, 0);
+const cv::Mat H9 = (cv::Mat_<double>(3, 3) << 1, 1, 1, 1, -9, 1, 1, 1, 1);
 
-    void HistogramEqualization(const cv::Mat &input_img, cv::Mat &output_img);
-    void CreateGaussBlurFilter(cv::Mat &filter, const double &sigma, int radim = -1);
-    void CreateGaussBlurFilter(cv::Mat &filter, const double &sigma, int radim_h,
-                               int radim_w);
-    void ImgFilter(const cv::Mat &img, const cv::Mat &filter, cv::Mat &dimg, const bool &is_resverse = false);
-    cv::Mat ConvertComplexMat2doubleMat(const cv::Mat &img);
-    template <typename T>
-    cv::Mat ConvertSingleChannelMat2ComplexMat(const cv::Mat &img);
-    cv::Mat ConvertDoubleMat2Uint8Mat(const cv::Mat &img);
-    
-    // 定义傅里叶变换的函数声明
-    void DFT(const cv::Mat &src, cv::Mat &dst);
-    void IDFT(const cv::Mat &src, cv::Mat &dst);
+inline double Dist(double x1, double y1, double x2, double y2) {
+  return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+struct ButterworthFilter {
+  double D0;
+  int n;
+  double W;
+  std::pair<double, double> center;
+  ButterworthFilter(std::pair<double, double> center, double D0 = 10, int n = 2,
+              double W = 1.0)
+      : center(center), D0(D0), n(n), W(W) {}
+  double LowPassFilter(double x, double y) {
+    double D = Dist(x, y, center.first, center.second);
+    return 1.0 / (1.0 + pow(D / D0, 2 * n));
+  }
+  double HighPassFilter(double x, double y) {
+    double D = Dist(x, y, center.first / 2, center.second / 2);
+    return 1.0 / (1.0 + pow(D0 / D, 2 * n));
+  }
+  double BandPassFilter(double x, double y) {
+    double D = Dist(x, y, center.first / 2, center.second / 2);
+    return 1.0 / (1.0 + pow(D * W / (D * D - D0 * D0), 2 * n));
+  }
+};
+
+struct GaussianFilter {
+  double D0;
+  std::pair<double, double> center;
+  GaussianFilter(std::pair<double, double> center, double D0 = 1.0)
+      : center(center), D0(D0) {}
+  double LowPassFilter(double x, double y) {
+    double D = Dist(x, y, center.first, center.second);
+    return exp(-D * D / (2 * D0 * D0));
+  }
+  double HighPassFilter(double x, double y) {
+    double D = Dist(x, y, center.first / 2, center.second / 2);
+    return 1 - exp(-D * D / (2 * D0 * D0));
+  }
+};
+
+void HistogramEqualization(const cv::Mat &input_img, cv::Mat &output_img);
+void CreateGaussBlurFilter(cv::Mat &filter, const double &sigma,
+                           int radim = -1);
+void CreateGaussBlurFilter(cv::Mat &filter, const double &sigma, int radim_h,
+                           int radim_w);
+void ImgFilter(const cv::Mat &img, const cv::Mat &filter, cv::Mat &dimg,
+               const bool &is_resverse = false);
+cv::Mat ConvertComplexMat2doubleMat(const cv::Mat &img);
+template <typename T>
+cv::Mat ConvertSingleChannelMat2ComplexMat(const cv::Mat &img);
+cv::Mat ConvertDoubleMat2Uint8Mat(const cv::Mat &img);
+
+// 定义傅里叶变换的函数声明
+void DFT(const cv::Mat &src, cv::Mat &dst);
+void IDFT(const cv::Mat &src, cv::Mat &dst);
 } // namespace MY_IMG
 template <typename T>
 cv::Mat MY_IMG::ConvertSingleChannelMat2ComplexMat(const cv::Mat &img) {
--- test/img_dft.cc
+++ test/img_dft.cc
@@ -1,62 +1,164 @@
-#include <iostream>
-#include <ctime>
 #include "img_io.h"
 #include "unit.h"
+#include <ctime>
+#include <iostream>
 
 using namespace MY_IMG;
 
-int main(int argc, char **argv)
-{
-    std::string file_name = "./test_img.png";
-    std::string output_name = "./test_img_out.png";
-    if (argc >= 2)
-    {
-        file_name = std::string(argv[1]);
-        if (argc >= 3)
-        {
-            output_name = std::string(argv[2]);
-        }
+int main(int argc, char **argv) {
+  std::string file_name = "./test_img.png";
+  std::string output_name = "./test_img_out.png";
+  if (argc >= 2) {
+    file_name = std::string(argv[1]);
+    if (argc >= 3) {
+      output_name = std::string(argv[2]);
     }
-    ImageIO img(file_name);
-    // get dft
-    cv::Mat dft_img;
-    cv::Mat gray_img;
-    img.rgb2gray(gray_img);
-    time_t t1 = clock();
-    DFT(gray_img, dft_img);
-    time_t t2 = clock();
-    LOG("DFT time: %f", static_cast<double>(t2 - t1) / 1000.0);
-    // show
-    cv::imshow("dft_img",ConvertComplexMat2doubleMat(dft_img));
+  }
+  ImageIO img(file_name);
+  // get dft
+  cv::Mat dft_img;
+  cv::Mat gray_img;
+  img.rgb2gray(gray_img);
+  time_t t1 = clock();
+  DFT(gray_img, dft_img);
+  time_t t2 = clock();
+  LOG("DFT time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("dft_img", ConvertComplexMat2doubleMat(dft_img));
 
-    // 创建一个理想低通滤波器
-    cv::Mat butter_filter = cv::Mat::zeros(gray_img.size(), CV_64FC1);
-    auto Dist = [x0 = gray_img.rows/2,y0 = gray_img.cols/2](int x, int y) {
-        return sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0));
-    };
-    // 设置截止频率和阶数
-    double D0 = 10;
-    int n = 2;
-    auto Butterworth = [Dist, &n, &D0](int x, int y) {
-        return 1.0 / (1.0 + pow(Dist(x, y) / D0, 2 * n));
-    };
-    // 进行滤波
-    for (int i = 0;i<gray_img.rows;i++)
-    {
-        for (int j = 0;j<gray_img.cols;j++)
-        {
-            dft_img.at<std::complex<double>>(i, j) *= Butterworth(i, j);
-        }
+  // 创建巴特沃斯滤波器
+  auto butter_filter =
+      ButterworthFilter({gray_img.rows / 2, gray_img.cols / 2});
+  cv::Mat butter_img_low(dft_img.size(), dft_img.type()),
+      butter_img_high(dft_img.size(), dft_img.type()),
+      butter_img_band(dft_img.size(), dft_img.type());
+  // 进行滤波
+  for (int i = 0; i < gray_img.rows; i++) {
+    for (int j = 0; j < gray_img.cols; j++) {
+      butter_img_low.at<std::complex<double>>(i, j) =
+          dft_img.at<std::complex<double>>(i, j) *
+          butter_filter.LowPassFilter(i, j);
     }
-    // get idft
-    cv::Mat idft_img;
-    t1 = clock();
-    IDFT(dft_img, idft_img);
-    t2 = clock();
-    LOG("IDFT time: %f", static_cast<double>(t2 - t1) / 1000.0);
-    // show
-    cv::imshow("idft_img", idft_img);
+  }
+  butter_filter.D0 = 60;
+  for (int i = 0; i < gray_img.rows; i++) {
+    for (int j = 0; j < gray_img.cols; j++) {
+      butter_img_high.at<std::complex<double>>(i, j) =
+          dft_img.at<std::complex<double>>(i, j) *
+          butter_filter.HighPassFilter(i, j);
+    }
+  }
+  for (int i = 0; i < gray_img.rows; i++) {
+    for (int j = 0; j < gray_img.cols; j++) {
+      butter_img_band.at<std::complex<double>>(i, j) =
+          dft_img.at<std::complex<double>>(i, j) *
+          butter_filter.BandPassFilter(i, j);
+    }
+  }
 
-    cv::waitKey(0);
-    return 0;
+  // 创建高斯滤波器
+  auto gauss_filter =
+      GaussianFilter({gray_img.rows / 2, gray_img.cols / 2}, 10);
+  cv::Mat gauss_img_low(dft_img.size(), dft_img.type()),
+      gauss_img_high(dft_img.size(), dft_img.type());
+  // 进行滤波
+  for (int i = 0; i < gray_img.rows; i++) {
+    for (int j = 0; j < gray_img.cols; j++) {
+      gauss_img_low.at<std::complex<double>>(i, j) =
+          dft_img.at<std::complex<double>>(i, j) *
+          gauss_filter.LowPassFilter(i, j);
+    }
+  }
+  gauss_filter.D0 = 60;
+  for (int i = 0; i < gray_img.rows; i++) {
+    for (int j = 0; j < gray_img.cols; j++) {
+      gauss_img_high.at<std::complex<double>>(i, j) =
+          dft_img.at<std::complex<double>>(i, j) *
+          gauss_filter.HighPassFilter(i, j);
+    }
+  }
+  // get idft
+  cv::Mat idft_img;
+  t1 = clock();
+  IDFT(dft_img, idft_img);
+  t2 = clock();
+  LOG("IDFT time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("idft_img", idft_img);
+
+  // get butterworth low pass
+  cv::Mat butter_low_img;
+  t1 = clock();
+  IDFT(butter_img_low, butter_low_img);
+  t2 = clock();
+  LOG("Butterworth low pass time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("butter_low_img", butter_low_img);
+
+  // get butterworth high pass
+  cv::Mat butter_high_img;
+  t1 = clock();
+  IDFT(butter_img_high, butter_high_img);
+  t2 = clock();
+  LOG("Butterworth high pass time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("butter_high_img", butter_high_img);
+
+  // get butterworth band pass
+  cv::Mat butter_band_img;
+  t1 = clock();
+  IDFT(butter_img_band, butter_band_img);
+  t2 = clock();
+  LOG("Butterworth band pass time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("butter_band_img", butter_band_img);
+
+  // get gauss low pass
+  cv::Mat gauss_low_img;
+  t1 = clock();
+  IDFT(gauss_img_low, gauss_low_img);
+  t2 = clock();
+  LOG("Gauss low pass time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("gauss_low_img", gauss_low_img);
+
+  // get gauss high pass
+  cv::Mat gauss_high_img;
+  t1 = clock();
+  IDFT(gauss_img_high, gauss_high_img);
+  t2 = clock();
+  LOG("Gauss high pass time: %f", static_cast<double>(t2 - t1) / 1000.0);
+  // show
+  cv::imshow("gauss_high_img", gauss_high_img);
+
+  // 拆分通道
+  cv::Mat img_one[3];
+  cv::split(img.GetData(), img_one);
+  gauss_filter.D0 = 10;
+  // 将三个通道单独使用高通滤波器进行滤波
+  cv::Mat img_one_dft[3];
+  cv::Mat img_one_dft_low[3];
+  cv::Mat img_one_low[3];
+  for (int channe = 0; channe < 3; channe++) {
+    // 单独对每个通道进行傅里叶变换
+    DFT(img_one[channe], img_one_dft[channe]);
+    img_one_dft_low[channe] =
+        cv::Mat(img_one_dft[channe].size(), img_one_dft[channe].type());
+    for (int i = 0; i < img_one[channe].rows; i++) {
+      for (int j = 0; j < img_one[channe].cols; j++) {
+        img_one_dft_low[channe].at<std::complex<double>>(i, j) =
+            img_one_dft[channe].at<std::complex<double>>(i, j) *
+            gauss_filter.LowPassFilter(i, j);
+      }
+    }
+    // 将三个通道的高通滤波结果进行IDFT
+    IDFT(img_one_dft_low[channe], img_one_low[channe]);
+  }
+  // 将三个通道的高通滤波结果进行合并
+  cv::Mat img_one_low_merge;
+  cv::merge(img_one_low, 3, img_one_low_merge);
+  cv::imshow("img_one_low_merge", img_one_low_merge);
+
+  cv::waitKey(0);
+  return 0;
 }
